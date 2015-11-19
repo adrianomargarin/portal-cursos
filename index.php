436dfc644ed5dae33e5dc330ebe87b29a2045668
Adriano Margarin <adriano.margarin@gmail.com>
2015-11-18 23:05:40 -0200

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Portal de Cursos</title>
+        <link rel="stylesheet" type="text/css" href="static/css/bootstrap.min.css">
+        <script type="text/javascript" src="static/js/jquery.min.js"></script>
+        <script type="text/javascript" src="static/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="static/js/trabalho.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <p class="navbar-text">Portal de Cursos</p>
+            </div>
+        </nav>
+        </div>
+    </body>
+</html>
